5e95d3aca05e67863c58a0e4277b0ab29683d611
Hanna Zelis <hannazelis@gmail.com>
2025-05-25 13:15:39 -0500

    made frameowrk of folders and files, setup the envrionment, and made requirements.txt

---
+++ setup_env.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+# --- Setup Envrionment Script ---
+ENV_NAME = "fitme-env"
+PYTHON_VERSION = "3.9"
+
+echo "Creating Conda envrionment '$ENV_NAME'..."
+
+
+
+# --- Initialize Conda
+CONDA_BASE = "$HOME/miniconda3"
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+
+
+
+# --- Create Envrionment (if not exists) ---
+if conda info --envs | grep -q "$ENV_NAME"; then
+    echo "Conda environment '$ENV_NAME' already exists. Skipping creation."
+else
+    echo "New Conda environment '$ENV_NAME' is being created with Python $PYTHON_VERSION."
+    conda create -y -n "$ENV_NAME" python="$PYTHON_VERSION"
+fi
+
+
+
+# --- Activate Envrionment ---
+conda activate $ENV_NAME
+echo "Envrionment '$ENV_NAME' activated."
+
+
+
+# --- Install Requirements ---
+echo "Installing requirements..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Setup complete for environment '$ENV_NAME'."
